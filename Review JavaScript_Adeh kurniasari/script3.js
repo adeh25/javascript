146c5ad6de7c7df87c8375040a9023587857493d
adeh25 <adehkurniasari09@gmail.com>
2024-04-16 14:08:55 +0700

    ReviewJavascript

--- Review JavaScript_Adeh kurniasari/script3.js
+++ Review JavaScript_Adeh kurniasari/script3.js
@@ -1,6 +1,6 @@
-const perkalian=prompt("Silahkan input angka")
-console.log("Berikut tabel perkalian "+perkalian)
-for (let a = 1;a <= 10; a++) {
-    let jumlah = perkalian * a
-console.log(perkalian +" x "+ a +" = "+jumlah)
+const perkalian = prompt("Silahkan input angka");
+console.log("Berikut tabel perkalian " + perkalian);
+for (let a = 1; a <= 10; a++) {
+  let jumlah = perkalian * a;
+  console.log(perkalian + " x " + a + " = " + jumlah);
 }
